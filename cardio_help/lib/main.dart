978e05528970febda452c5487b31ddb803093f2a
Aline Lima <lima_aline@id.uff.br>
2019-12-19 22:19:40 -0300

    Update main.dart

--- cardio_help/lib/main.dart
+++ cardio_help/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Cardio Help'),
+      home: MyHomePage(title: 'Cardio help'),
     );
   }
 }
