2d0543af8c2ca87120b5881e23ddae5e852af944
Aline Lima <lima_aline@id.uff.br>
2020-01-12 19:36:33 -0300

    Criação do Drawer Customizado
    Foi implementado o drawer informativo que se encontra dentro da página de medicamentos.

--- cardio_help/lib/pages/DrugsPage/drugsPage.dart
+++ cardio_help/lib/pages/DrugsPage/drugsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cardio_help/objects/custom_drawer/custom_navigator_drawer.dart';
 
 class DrugsPage extends StatefulWidget {
   final Map medicamento;
@@ -18,61 +19,12 @@ class _DrugsPageState extends State<DrugsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child : Scaffold(
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text(widget.medicamento["name"]),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Detalhes'),
-              Tab(text: 'Indicação e Cuidados'),
-              ],
-          ),
         ),
-        body: TabBarView(
-          children: [
-            // Aba de detalhes: devem aparecer essas chaves: presentation, dilution, routeAdm
-            Column(
-              children: <Widget>[
-                Text("Diluição"),
-                Text(widget.medicamento["dilution"]),
-                Text("Apresentação"),
-                Column(
-                  children: List.generate(widget.medicamento["presentation"].length, (index){
-                    return Text(widget.medicamento["presentation"][index].toString());
-                  })
-                ),
-                Text("Rota de Administração"),
-                Column(
-                  children: List.generate(widget.medicamento["routeAdm"].length, (index){
-                    return Text(widget.medicamento["routeAdm"][index].toString());
-                  })
-                )
-              ]
-            )
-            ,
-            // Aba de Indicação e Cuidados: devem aparecer essas chaves: indication, care
-            Column(
-              children: <Widget>[
-                Text("Indicação"),
-                Column(
-                  children:List.generate(widget.medicamento["indication"].length, (index){
-                    return Text(widget.medicamento["indication"][index].toString());
-                  })
-                ),
-                Text("Cuidados"),
-                Column(
-                  children: List.generate(widget.medicamento["care"].length, (index){
-                    return Text(widget.medicamento["care"][index].toString());
-                  })
-                )
-              ]
-            )
-          ],
-        ),
-      ),
+        body: CollapsingNavigationDrawer()
     );
   }
 }
