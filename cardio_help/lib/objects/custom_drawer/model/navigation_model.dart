b12c9c2bd0e1dccc5c79ee26e9f1781cf31bdaec
debsk <57530923+debsk@users.noreply.github.com>
2020-01-14 21:51:06 -0300

    Alteraçao dos ícones do drawer.
    Mudança dos ícones : evento adverso e interação.

--- cardio_help/lib/objects/custom_drawer/model/navigation_model.dart
+++ cardio_help/lib/objects/custom_drawer/model/navigation_model.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cardio_help/objects/custom_drawer/model/my_flutter_app_icons.dart';
+import 'package:cardio_help/objects/custom_drawer/model/attention_icons.dart';
 
 class NavigationModel {
   String title;
@@ -7,9 +9,10 @@ class NavigationModel {
   NavigationModel({this.title, this.icon});
 }
 
-List<NavigationModel> navigationItems = [
-  NavigationModel(title: "Classificação", icon: Icons.format_list_bulleted),
-  NavigationModel(title: "Evento Adverso", icon: Icons.error),
-  NavigationModel(title: "Uso Terapêutico", icon: Icons.healing),
-  NavigationModel(title: "Interação", icon: Icons.notifications),
-];
+  List<NavigationModel> navigationItems = [
+    NavigationModel(title: "Classificação", icon: Icons.format_list_bulleted),
+    NavigationModel(title: "Evento Adverso", icon: MyFlutterApp.interactive),
+    NavigationModel(title: "Uso Terapêutico", icon: Icons.healing),
+    NavigationModel(title: "Interação", icon: Attention.attention__1_),
+  ];
+
