40ec069a392f5a0360d48ce561295b39bd79161b
Karina <karinalemos@id.uff.br>
2020-01-19 15:33:15 -0300

    Melhora no designer dos textos e ajustes na integração

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # cardio_help
-Projeto de extensão desenvolvido pelo GCAS-UFF
 
-O Cardio Help é um aplicativo com o objetivo de auxiliar enfermeiros e outros profissionais de saúde que trabalham em Centros de Terapia Intensiva (CTI), informando características sobre medicamentos cardiotônicos e suas principais interações com outras drogas.
+A new Flutter project.
 
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/pages/DrugsPage/drugsPage.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:cardio_help/objects/custom_drawer/custom_navigator_drawer.dart';
+import 'package:cardio_help/objects/presentation.dart';
+import 'package:cardio_help/objects/custom_drawer/commons/currentIndex.dart';
+
+class DrugsPage extends StatefulWidget {
+  final Map medicamento;
+  final Color backgroundColor;
+  final bool flag;
+
+  const DrugsPage(
+    { 
+      Key key,
+      @required this.flag,
+      @required this.medicamento,
+      @required this.backgroundColor,
+    }
+  ): super(key: key); 
+
+  @override     
+  _DrugsPageState createState() => _DrugsPageState();
+}
+
+class _DrugsPageState extends State<DrugsPage> {
+
+  Map getData(int index, bool flag){
+    Map data = {
+      "title": null,
+      "infos": null
+    };
+
+    if(flag){
+      if(index == 0){
+        data["title"] = ["Indicação","Apresentação","Diluição", "Rota de Administração"];
+        data["infos"] = [
+          this.widget.medicamento["indication"],
+          this.widget.medicamento["presentation"],
+          this.widget.medicamento["dilution"],
+          this.widget.medicamento["routeAdm"]
+        ];
+      }else if(index == 1){
+        data["title"] = ["Cuidados"];
+        data["infos"] = [
+          this.widget.medicamento["care"]
+        ];
+      }
+    }else{
+      if(index == 0){
+        data["title"] = ["Classificação"];
+        data["infos"] = [
+          this.widget.medicamento["classification"]
+        ];
+      }else if(index == 1){
+        data["title"] = ["Evento Adverso"];
+        data["infos"] = [
+          this.widget.medicamento["adverseReaction"]
+        ];
+      }else if(index == 2){
+        data["title"] = ["Uso Terapêutico"];
+        data["infos"] = [
+          this.widget.medicamento["therapeuticUse"]
+        ];
+      }else if(index == 3){
+        data["title"] = ["Interações Medicamentosas"];
+        data["infos"] = [
+          this.widget.medicamento["interactionMed"],
+          this.widget.medicamento["interactionEffectEffect"],
+        ];
+      }
+    }
+    return data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: widget.backgroundColor,
+          title: Text(widget.medicamento["name"]),
+        ),
+        body: Stack(
+          children: <Widget>[  
+            
+            Container(
+              margin: EdgeInsets.only(left: 80),
+              
+              child: Presentation(
+                data: this.getData(currentSelectedIndex, widget.flag),
+              ),
+            ),
+            
+            CollapsingNavigationDrawer(
+              drawerBackgroundColor: widget.backgroundColor,
+              flag: this.widget.flag,
+            )
+          ]
+        )
+    );
+  }
+}
