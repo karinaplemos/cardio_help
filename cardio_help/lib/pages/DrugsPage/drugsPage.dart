20b9026346d94b701b7cc729a4afd2ebaf37fa95
Aline <lima_aline@id.uff.br>
2020-02-09 21:52:27 -0300

    Criação do Grid Buttons
    Foram removidas as abas horizontais de 'Detalhes sobre o medicamento' e 'Informações' dentro da página DrugsPage.
    
    Foram criados os botões da página DrugsPage.

--- cardio_help/lib/pages/DrugsPage/drugsPage.dart
+++ cardio_help/lib/pages/DrugsPage/drugsPage.dart
@@ -15,64 +15,92 @@ class DrugsPage extends StatefulWidget {
 }
 
 class _DrugsPageState extends State<DrugsPage> {
+  List topicos = ['Classificação/Efeitos','Uso Terapêutico','Eventos Adversos', 'Detalhes', 'Interação Medicamentosa','Cuidados'];
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child : Scaffold(
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text(widget.medicamento["name"]),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Detalhes'),
-              Tab(text: 'Indicação e Cuidados'),
-              ],
-          ),
         ),
-        body: TabBarView(
-          children: [
-            // Aba de detalhes: devem aparecer essas chaves: presentation, dilution, routeAdm
-            Column(
-              children: <Widget>[
-                Text("Diluição"),
-                Text(widget.medicamento["dilution"]),
-                Text("Apresentação"),
-                Column(
-                  children: List.generate(widget.medicamento["presentation"].length, (index){
-                    return Text(widget.medicamento["presentation"][index].toString());
-                  })
+        body:  GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            crossAxisCount: 2,
+            children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+
+                  },
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Classificação/Efeitos'),
+                  color: Colors.teal[100],
                 ),
-                Text("Rota de Administração"),
-                Column(
-                  children: List.generate(widget.medicamento["routeAdm"].length, (index){
-                    return Text(widget.medicamento["routeAdm"][index].toString());
-                  })
-                )
-              ]
-            )
-            ,
-            // Aba de Indicação e Cuidados: devem aparecer essas chaves: indication, care
-            Column(
-              children: <Widget>[
-                Text("Indicação"),
-                Column(
-                  children:List.generate(widget.medicamento["indication"].length, (index){
-                    return Text(widget.medicamento["indication"][index].toString());
-                  })
+                RaisedButton(
+                  onPressed: () {
+
+                  },
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Uso Terapêutico'),
+                  color: Colors.teal[200],
                 ),
-                Text("Cuidados"),
-                Column(
-                  children: List.generate(widget.medicamento["care"].length, (index){
-                    return Text(widget.medicamento["care"][index].toString());
-                  })
-                )
-              ]
-            )
-          ],
-        ),
+                RaisedButton(
+                  onPressed: () {
+
+                  },
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Eventos Adversos'),
+                  color: Colors.teal[300],
+                ),
+                RaisedButton(
+                  onPressed: () {
+
+                  },
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Detalhes'),
+                  color: Colors.teal[400],
+                ),
+                RaisedButton(
+                  onPressed: () {
+
+                  },
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Interação Medicamentosa'),
+                  color: Colors.teal[500],
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    
+                  },
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Cuidados'),
+                  color: Colors.teal[600],
+                ),
+            ],
       ),
-    );
+
+        //        Text("Diluição"),
+        //        Text(widget.medicamento["dilution"]),
+        //        Text("Apresentação"),
+        //        Column(
+       //           children: List.generate(widget.medicamento["presentation"].length, (index){
+       //             return Text(widget.medicamento["presentation"][index].toString());
+       //           })
+       //         ),
+       //         Text("Rota de Administração"),
+        //        Column(
+       //           children: List.generate(widget.medicamento["routeAdm"].length, (index){
+         //           return Text(widget.medicamento["routeAdm"][index].toString() );
+                 // })
+            //    )          
+  );   
   }
+
+  returnWidget( ){
+
+  }
+
 }
